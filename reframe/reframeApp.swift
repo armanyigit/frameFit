057f7db74962b001a60d6046e17e4301238ea101
arman yigit <armanyigit@gmail.com>
2024-12-10 22:52:54 +0100

    initial stable version

---
+++ reframe/reframeApp.swift
@@ -0,0 +1,25 @@
+//
+//  reframeApp.swift
+//  reframe
+//
+//  Created by Arman Yigit on 08.12.24.
+//
+
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
+
+@main
+struct reframeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ reframeTests/reframeTests.swift
@@ -0,0 +1,17 @@
+//
+//  reframeTests.swift
+//  reframeTests
+//
+//  Created by Arman Yigit on 08.12.24.
+//
+
+import Testing
+@testable import reframe
+
+struct reframeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
